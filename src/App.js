433238b67610965542e7296bbe452b7e494fc24e
Michael Gad <mokai2008@gmail.com>
2018-08-28 00:51:36 +0200

    construct the app main file

--- src/App.js
+++ src/App.js
@@ -1,21 +1,198 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import RestaurantsList from './RestaurantsList';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+
+    constructor(props) {
+        super(props);
+        this.state = {
+          // instead of making an individual JSON file
+            'alllocations': [
+                {
+                    'name': "Trendz",
+                    'latitude': 31.268759,
+                    'longitude': 32.316086
+                },
+                {
+                    'name': "Pizza Hut",
+                    'latitude': 31.267691,
+                    'longitude': 32.3128318
+                },
+                {
+                    'name': "Mcdonald's",
+                    'latitude': 31.270596,
+                    'longitude': 32.3112684
+                },
+                {
+                    'name': "Portobello",
+                    'latitude': 31.2724017,
+                    'longitude': 32.2940547
+                },
+                {
+                    'name': "Quick",
+                    'latitude': 31.2710454,
+                    'longitude': 32.3043259
+                },                                                                
+            ],
+            'map': '',
+            'infowindow': '',
+            'prevmarker': ''
+        };
+
+        // getting object instance when used in the function
+
+        this.initMap = this.initMap.bind(this);
+        this.openInfoWindow = this.openInfoWindow.bind(this);
+        this.closeInfoWindow = this.closeInfoWindow.bind(this);
+    }
+
+    componentDidMount() {
+
+        // Connect the initMap() function within this class to the global window context,
+
+          window.initMap = this.initMap;
+
+        // Loading the Map API
+        
+        loadScript('https://maps.googleapis.com/maps/api/js?key=AIzaSyCPi0o_tjNjKYYDe_6nYg82r0leI7kKlOE&callback=initMap')
+    }
+
+    // Google Map Function Intitalization
+
+    initMap() {
+        var self = this;
+
+        var mapview = document.getElementById('map');
+        mapview.style.height = window.innerHeight + "px";
+        var map = new window.google.maps.Map(mapview, {
+            center: {lat: 31.2652893, lng: 32.3018661},
+            zoom: 15,
+            mapTypeControl: false
+        });
+
+        var InfoWindow = new window.google.maps.InfoWindow({});
+
+        window.google.maps.event.addListener(InfoWindow, 'closeclick', function () {
+            self.closeInfoWindow();
+        });
+
+        this.setState({
+            'map': map,
+            'infowindow': InfoWindow
+        });
+
+        window.google.maps.event.addDomListener(window, "resize", function () {
+            var center = map.getCenter();
+            window.google.maps.event.trigger(map, "resize");
+            self.state.map.setCenter(center);
+        });
+
+        window.google.maps.event.addListener(map, 'click', function () {
+            self.closeInfoWindow();
+        });
+
+        var alllocations = [];
+        this.state.alllocations.forEach(function (location) {
+            var longname = location.name + ' - ' + 'Restaurant';
+            var marker = new window.google.maps.Marker({
+                position: new window.google.maps.LatLng(location.latitude, location.longitude),
+                animation: window.google.maps.Animation.DROP,
+                map: map
+            });
+
+            marker.addListener('click', function () {
+                self.openInfoWindow(marker);
+            });
+
+            location.longname = longname;
+            location.marker = marker;
+            location.display = true;
+            alllocations.push(location);
+        });
+        this.setState({
+            'alllocations': alllocations
+        });
+    }
+
+    // Poping up the InfoWindow
+
+    openInfoWindow(marker) {
+        this.closeInfoWindow();
+        this.state.infowindow.open(this.state.map, marker);
+        marker.setAnimation(window.google.maps.Animation.BOUNCE);
+        this.setState({
+            'prevmarker': marker
+        });
+        this.state.infowindow.setContent('Loading Data...');
+        this.state.map.setCenter(marker.getPosition());
+        this.state.map.panBy(0, -200);
+        this.getMarkerInfo(marker);
+    }
+
+    // Getting the Restaurant data from FourSquare 
+
+    getMarkerInfo(marker) {
+        var self = this;
+        var clientId = "TPIDDHBKB2QFBWEV2MPDOFGUSWXCXGAA5IVOWEMN5ASR3UJW";
+        var clientSecret = "4HB1ZZJBVXC3F0BREBPSGXYK0VZ5ALS4XRNJZSBP1JROG0DE";
+        var url = "https://api.foursquare.com/v2/venues/search?client_id=" + clientId 
+        + "&client_secret=" + clientSecret + "&v=20130815&ll=" + marker.getPosition().lat() + "," + marker.getPosition().lng() + "&limit=1";
+        fetch(url)
+            .then(
+                function (response) {
+                    if (response.status !== 200) {
+                        self.state.infowindow.setContent("Sorry data can't be loaded");
+                        return;
+                    }
+
+                    // response information
+                    response.json().then(function (data) {
+                        var location_data = data.response.venues[0];
+                        var checkinsCount = '<b>Number of CheckIn: </b>' + location_data.stats.checkinsCount + '<br>';
+                        var readMore = '<a href="https://foursquare.com/v/'+ location_data.id +'" target="_blank">More information at Foursquare Website</a>'
+                        self.state.infowindow.setContent(checkinsCount  + readMore);
+                    });
+                }
+            )
+            .catch(function (err) {
+                self.state.infowindow.setContent("Sorry data can't be loaded");
+            });
+    }
+
+    // Closing the Info Window
+
+    closeInfoWindow() {
+        if (this.state.prevmarker) {
+            this.state.prevmarker.setAnimation(null);
+        }
+        this.setState({
+            'prevmarker': ''
+        });
+        this.state.infowindow.close();
+    }
+
+    // Rendering the main function of the APp
+
+    render() {
+        return (
+            <div>
+                <RestaurantsList key="100" alllocations={this.state.alllocations} openInfoWindow={this.openInfoWindow}
+                              closeInfoWindow={this.closeInfoWindow}/>
+                <div id="map"></div>
+            </div>
+        );
+    }
 }
 
 export default App;
+
+// Putting the Google Map API Script
+
+function loadScript(url) {
+  var index = window.document.getElementsByTagName('script')[0]
+  var script = window.document.createElement('script')
+  script.src = url
+  script.async = true
+  script.defer = true
+  index.parentNode.insertBefore(script, index)
+}
